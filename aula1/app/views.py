253c5f51e94c652c8038e16b9ad9687b986a9eaa
Joaquim-Renato <jocajoaquimas@gmail.com>
2024-10-02 17:04:21 -0300

    subindo alterações

--- aula1/app/views.py
+++ aula1/app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+from .models import email 
 
-# Create your views here.
+def login_user(request):
+    return 
+
+def logout_user(request):
+    return
+
+def check_login(request):
+    return
