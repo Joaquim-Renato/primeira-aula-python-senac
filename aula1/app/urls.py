b1cd17b1ca8257d22de1cbb1a145695e33ef133e
Joaquim-Renato <jocajoaquimas@gmail.com>
2024-10-04 16:56:29 -0300

    atualizandooooo

---
+++ aula1/app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import login_user, check_login
+
+urlpatterns = [
+    path("", login_user, name = "index"),
+    path("/home", check_login, name="home")
+]
