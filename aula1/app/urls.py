ee31c0dbdd7c6473cdafd7685ad5b9b13e32bd30
Joaquim-Renato <jocajoaquimas@gmail.com>
2024-10-09 15:14:43 -0300

    subindo alterações

--- aula1/app/urls.py
+++ aula1/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login_user, check_login
+from .views import login_user, check_login, logout_user
 
 urlpatterns = [
     path("", login_user, name = "index"),
-    path("home/", check_login, name="home")
+    path("home/", check_login, name="home"),
+    path("logout/", logout_user, name="logout")
 ]
--- aula1/app/views.py
+++ aula1/app/views.py
@@ -2,22 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import User
+from django.contrib.auth import logout
 
 def login_user(request):
-    if request.method == "post": 
-        userName = request.post["email"]
-        userPassword = request.post["password"]
+    if request.method == "POST": 
+        userName = request.POST["email"]
+        userPassword = request.POST["password"]
+
         try:
             success = User.objects.get(user = userName, password = userPassword)
             request.session["userID"] = success.id
             return redirect("home")
-        except:
-            User.DoesNotExist
-            messages.error("usuario e/ou senha inválidos!")
+        except User.DoesNotExist:
+            messages.error(request, "usuario e/ou senha inválidos!")
     return render(request, "index.html")
-
-def logout_user(request):
-    return
 
 def check_login(request):
     if "userID" in request.session:
@@ -25,3 +23,8 @@ def check_login(request):
     else:
         return redirect("index")
     
+def logout_user(request):
+        logout(request)
+        request.session.flush()
+        return redirect("index")
+  
